27048dc6f00fa68e3149b1457bdfb4f0bfe8a544
Justin <justinjchou@gmail.com>
2023-07-07 02:32:23 -0700

    exercise 1-18

--- main.c
+++ main.c
@@ -1,30 +1,72 @@
 #include <stdio.h>
+#define MAXLINE 1000 /* maximum input line size */
 
-int conversion(int lowerLimit, int upperLimit)
-{
-	float fahr, celsius;
-	int lower, upper, step;
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 
-	lower = lowerLimit;
-	upper = upperLimit;
-	step = 20;
-
-	fahr = lower;
-	while(fahr <= upper)
-	{
-		celsius = (5.0/9.0) * (fahr-32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
-	}
-}
-
+/* print longest input line */
 main()
 {
-	int lowerLimit, upperLimit;
-	scanf("%d", &lowerLimit);
-	scanf("%d", &upperLimit);
+	int len; // current line length
+	int max; /* maximum length seen so far */
+	char line [MAXLINE]; /*current input line*/
+	char longest [MAXLINE]; /* longest line saved here */
 
-	conversion (lowerLimit, upperLimit);
+	void removeTrailingWhitespace(char line[], int len);
+
+	max = 0;
+	// while there's another line 
+	while ((len = getline(line, MAXLINE)) > 0)
+		if (len > max)
+		{
+			max = len;
+			copy(longest, line);
+		}
+	
+	if (max > 0) /* there was a line */
+		printf("%s", longest);
+	return 0;
 }
 
+// this reads a line into s then returns the length
+int getline(char s[], int lim)
+{
+	int c, i;
 
+	// for longest line and not end of file, string s is set to char
+	// sets the second line
+	for (i=0; i < lim -1 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	// determines if end of line adds the character
+	if (c=='\n') {
+		s[i] = c;
+		++i;
+	}
+
+	s[i] = '\0';
+
+	return i;
+}
+
+/* copy: copy from 'from' into 'to'; assume to is big enough*/
+void copy (char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+			++i;
+}
+
+void removeTrailingWhitespace(char line[], int length)
+{
+	int i;
+
+	for (i = length -2; i >= 0; --i)
+	{
+		if (line[i] == ' ' || line[i] == '\t')
+			line[i] = '\0';
+		else 
+			break;
+	}
+}
